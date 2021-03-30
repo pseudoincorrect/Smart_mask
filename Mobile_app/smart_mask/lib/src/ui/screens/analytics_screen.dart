f9626705f66ef731a07dca47064dfbe23d9e6036
Maxime <maxime-clement@live.fr>
2021-03-30 16:07:00 +0300

    added the filter select widget

--- Mobile_app/smart_mask/lib/src/ui/screens/analytics_screen.dart
+++ Mobile_app/smart_mask/lib/src/ui/screens/analytics_screen.dart
@@ -25,6 +25,7 @@ class AnalyticsScreen extends StatelessWidget {
                 // ),
               ),
               IntervalSlider(),
+              FilterSelect(),
             ],
           ),
         ),
--- Mobile_app/smart_mask/lib/src/ui/widgets/analytics_widget.dart
+++ Mobile_app/smart_mask/lib/src/ui/widgets/analytics_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:smart_mask/src/logic/database/models/sensor_model.dart';
 
 import 'package:smart_mask/src/logic/blocs/analytics/analytics_bloc.dart';
@@ -86,30 +87,43 @@ class _IntervalSliderState extends State<IntervalSlider> {
   @override
   Widget build(BuildContext context) {
     sensorDataBloc = AnalyticsProvider.of(context);
-    return Card(
-      margin: EdgeInsets.all(10),
+    return Container(
+      margin: EdgeInsets.only(top: 10, left: 10, right: 10),
+      child: Card(
+        child: Container(
+          child: Column(
+            children: [
+              zoomAndTitle(),
+              SizedBox(height: 10),
+              timeSlider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget zoomAndTitle() {
+    return Container(
+      padding: EdgeInsets.only(left: 10, right: 10),
       child: Row(
-        children: [
+        children: <Widget>[
           Expanded(
-            flex: 8,
-            child: Column(
-              children: [
-                Container(child: Text("Navigate Your Data")),
-                Container(
-                  padding: EdgeInsets.only(top: 10),
-                  child: timeSlider(),
-                ),
-              ],
+            child: Container(
+              child: zoomOutButton(),
+              alignment: Alignment.centerLeft,
             ),
           ),
           Expanded(
-            flex: 2,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                zoomInButton(),
-                zoomOutButton(),
-              ],
+            child: Container(
+              child: Text("Navigate Your Data"),
+              alignment: Alignment.center,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: zoomInButton(),
+              alignment: Alignment.centerRight,
             ),
           ),
         ],
@@ -135,14 +149,83 @@ class _IntervalSliderState extends State<IntervalSlider> {
   Widget zoomInButton() {
     return ElevatedButton(
       onPressed: () => sensorDataBloc.increaseZoomLevel(),
-      child: Text("+"),
+      child: Icon(Icons.zoom_in),
     );
   }
 
   Widget zoomOutButton() {
     return ElevatedButton(
       onPressed: () => sensorDataBloc.decreaseZoomLevel(),
-      child: Text("-"),
+      child: Icon(Icons.zoom_out),
+    );
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+class FilterSelect extends StatelessWidget {
+  final double textInPutHeight = 40;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10),
+      child: Card(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          height: textInPutHeight * 2.5,
+          child: Column(
+            children: <Widget>[
+              Expanded(flex: 1, child: Text("Filters")),
+              Expanded(
+                flex: 3,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Expanded(
+                      child: filterCard("Low Pass", 1000),
+                    ),
+                    Expanded(
+                      child: filterCard("High Pass", 0.2),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget filterCard(String text, double value) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(width: 10),
+          Text("$text:"),
+          SizedBox(width: 10),
+          Container(
+            height: textInPutHeight,
+            width: 60,
+            child: TextField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.only(
+                  bottom: textInPutHeight / 2,
+                ),
+                hintText: value.toString(),
+              ),
+            ),
+          ),
+          SizedBox(width: 5),
+          Text("Hz"),
+          SizedBox(width: 10),
+        ],
+      ),
     );
   }
 }
